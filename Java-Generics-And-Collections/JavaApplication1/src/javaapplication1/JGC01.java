3ee52792a2f47aab7d8f42ee8c758b6b20994462
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-06-29 15:07:50 +0300

    Refactor code

--- Java-Generics-And-Collections/JavaApplication1/src/javaapplication1/JGC01.java
+++ Java-Generics-And-Collections/JavaApplication1/src/javaapplication1/JGC01.java
@@ -17,7 +17,7 @@ public class JGC01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        List<String> words = new ArrayList<>();
+        List<String> words = new ArrayList<String>();
         
         words.add("Hello"); words.add("world!");
         
