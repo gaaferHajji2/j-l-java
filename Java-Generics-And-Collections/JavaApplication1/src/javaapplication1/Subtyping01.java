5373a3725a7efd19bbe018578783ed79242c5eb2
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-07-10 15:47:04 +0300

    Testing subtype with autoboxing

---
+++ Java-Generics-And-Collections/JavaApplication1/src/javaapplication1/Subtyping01.java
@@ -0,0 +1,21 @@
+package javaapplication1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Subtyping01 {
+
+    
+    public static void main(String[] args) {
+        List<Number> ints = new ArrayList<>();
+        
+        ints.add(1);
+        ints.add(0.25);
+        
+        assert ints.equals(List.of(1, 0.25));
+        
+        System.out.println("OK 1!!");
+    }
+    
+}
