3ac9bfdb8ab6f682864c71066b2d4faf13c7e8dc
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-07-10 15:52:21 +0300

    Add comment

--- Java-Generics-And-Collections/JavaApplication1/src/javaapplication1/Subtyping01.java
+++ Java-Generics-And-Collections/JavaApplication1/src/javaapplication1/Subtyping01.java
@@ -10,6 +10,7 @@ public class Subtyping01 {
     public static void main(String[] args) {
         List<Number> ints = new ArrayList<>();
         
+        // Here we use autoboxing for types 1 --> Integer, and 0.25 --> Double
         ints.add(1);
         ints.add(0.25);
         
