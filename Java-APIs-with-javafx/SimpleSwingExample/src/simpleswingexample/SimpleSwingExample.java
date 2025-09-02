1441928d6c36054dd287242529adac5990f8de2f
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-09-02 10:34:35 +0300

    update close behavior

--- Java-APIs-with-javafx/SimpleSwingExample/src/simpleswingexample/SimpleSwingExample.java
+++ Java-APIs-with-javafx/SimpleSwingExample/src/simpleswingexample/SimpleSwingExample.java
@@ -9,8 +9,13 @@ public class SimpleSwingExample {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Simple Swing Example");
+            
+            //set size and location
             frame.setBounds(350, 350, 350, 350);
+            
             frame.setVisible(true);
+            
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         });
     }
     
