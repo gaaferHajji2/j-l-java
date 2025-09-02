c7b28e987793402c005440bed48011b4c83a1970
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-09-02 09:48:35 +0300

    Adding simple swing example

---
+++ Java-APIs-with-javafx/SimpleSwingExample/src/simpleswingexample/SimpleSwingExample.java
@@ -0,0 +1,14 @@
+package simpleswingexample;
+
+import javax.swing.JFrame;
+
+public class SimpleSwingExample {
+
+    
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Simple Swing Example");
+        
+        frame.setVisible(true);
+    }
+    
+}
