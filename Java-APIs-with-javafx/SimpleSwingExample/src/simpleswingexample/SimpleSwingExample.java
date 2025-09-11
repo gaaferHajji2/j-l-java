a5ca8c35892df122fc5add726c5749f1c0eb013e
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-09-11 15:20:22 +0300

    Adding JButton to Content Pane of frame

--- Java-APIs-with-javafx/SimpleSwingExample/src/simpleswingexample/SimpleSwingExample.java
+++ Java-APIs-with-javafx/SimpleSwingExample/src/simpleswingexample/SimpleSwingExample.java
@@ -1,5 +1,7 @@
 package simpleswingexample;
 
+import java.awt.Container;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
@@ -9,6 +11,12 @@ public class SimpleSwingExample {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Simple Swing Example");
+            
+            JButton closeButton = new JButton("Close");
+            
+            Container container = frame.getContentPane();
+            
+            container.add(closeButton);
             
             //set size and location
             frame.setBounds(350, 350, 350, 350);
